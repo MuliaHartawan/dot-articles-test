1d1b9de520cde77289c8e601c272dd862a3a7639
MuliaHartawan <mulia.hartawa011@gmail.com>
2024-10-04 15:50:32 +0700

    initial project preparation

---
+++ src/main.ts
@@ -0,0 +1,29 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('DOT Articles')
+    .setDescription('DOT Articles API Documentation')
+    .setVersion('1.0')
+    .addServer('/api/v1')
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('/api/v1/api-list', app, document, {
+    swaggerOptions: {
+      tagsSorter: 'alpha',
+      operationSorter: 'alpha',
+      docExpansion: 'none',
+      filter: true,
+    },
+  });
+
+  app.setGlobalPrefix('/api/v1');
+  await app.listen(3000);
+}
+bootstrap();
